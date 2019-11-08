e2257f67678a33f86398ccf7db9450a89f1b7c8d
Mikhail Aleksandrov <al-mn@yandex.ru>
2019-11-08 14:07:05 +0300

    post detail page

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from posts.views import index, blog, post, search
 
@@ -10,7 +10,9 @@ urlpatterns = [
     path('', index),
     path('blog/', blog, name='post-list'),
     path('search/', search, name='search'),
-    path('post/<id>/', post, name='post-detail')
+    path('post/<id>/', post, name='post-detail'),
+
+    path(r'^tinymce/', include('tinymce.urls'))
 ]
 
 if settings.DEBUG:
